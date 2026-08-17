7ab61cc224774c2b15b3eb640bfffd6c93a4a7ee
I Made Cakra Pustaka <90371209+Cakra17@users.noreply.github.com>
2026-08-17 17:42:50 +0800

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        stack = []
+        def dfs(i):
+            if i >= len(nums):
+                return
+            
+            if sum(stack) == target:
+                res.append(stack.copy())
+                return
+            
+            if sum(stack) < target:
+                stack.append(nums[i])
+                dfs(i)
+                stack.pop()
+            
+            dfs(i+1)
+        
+        dfs(0)
+        return res
