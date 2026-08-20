0504dbfb3769772ab37cfb3e532e479f86a66c48
I Made Cakra Pustaka <90371209+Cakra17@users.noreply.github.com>
2026-08-20 19:02:02 +0800

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        self.backtrack(0, nums)
+        return self.res
+
+    def backtrack(self, idx: int, nums: List[int]):
+        if idx == len(nums):
+            self.res.append(nums[:])
+            return
+
+        for i in range(idx, len(nums)):
+            nums[idx], nums[i] = nums[i], nums[idx]
+            self.backtrack(idx+1, nums)
+            nums[idx], nums[i] = nums[i], nums[idx]
